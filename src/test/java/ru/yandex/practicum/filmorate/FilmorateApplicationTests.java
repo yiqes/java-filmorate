9c1467d126f6660d89763fe05732c842ab111045
yiqes <alkomlev23@gmail.com>
2024-05-21 15:33:35 +0300

    added controllers and some tests 1st commit

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,12 +2,45 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class FilmorateApplicationTests {
+	@Test
+	public void testCreateFilmWithValidData() {
+		Film film = new Film();
+		FilmController filmController = new FilmController();
+		film.setName("Test Film");
+		film.setDescription("Test Description");
+		film.setReleaseDate(LocalDate.of(2020, 1, 1));
+		film.setDuration((120L));
+
+		Film createdFilm = filmController.create(film);
+
+		assertEquals(createdFilm.getName(), "Test Film");
+		assertEquals(createdFilm.getDescription(), "Test Description");
+		assertEquals(createdFilm.getReleaseDate(), LocalDate.of(2020, 1, 1));
+		assertEquals(createdFilm.getDuration(), 120);
+	}
 
 	@Test
-	void contextLoads() {
+	public void testCreateFilmWithInvalidName() {
+		Film film = new Film();
+		FilmController filmController = new FilmController();
+		film.setName(null);
+		film.setDescription("Test Description");
+		film.setReleaseDate(LocalDate.of(2020, 1, 1));
+		film.setDuration(120L);
+
+		assertThrows(ValidateException.class, () -> filmController.create(film));
 	}
+
+// Добавьте подобные тестовые методы для остальных путей и проверок
 
 }
