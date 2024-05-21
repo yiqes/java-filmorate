c3bfb0b965ffa25fadc55f1cea27e9370ed41242
yiqes <alkomlev23@gmail.com>
2024-05-21 15:39:18 +0300

    added controllers and some tests 1st commit deleted unused imports

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,12 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
 import ru.yandex.practicum.filmorate.ValidateException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
